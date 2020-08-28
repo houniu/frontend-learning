5b206d70b1704d2bdafcd7479229d6ff1a71d7bc
侯磊 <houlei02@meituan.com>
2020-08-28 08:27:56 +0800

    update Function.prototype.myApply

--- src/common/utils/index.js
+++ src/common/utils/index.js
@@ -92,23 +92,11 @@ export const cloneDeep = obj => {
   return new_Obj;
 };
 
-Function.prototype.myBind = function(context) {
-  if (typeof this !== 'function')
-    throw new TypeError(
-      'Function.prototype.bind - what is trying to be bound is not callable'
-    );
-  if (this === Function.prototype) throw new TypeError('error');
-  // 保存this
-  const _this = this;
-  const args = [...arguments].slice(1);
-  // 返回一个函数
-  return function() {
-    return _this.apply(context, args.concat(...arguments));
-  };
-};
-
 ~(function() {
   function myCall(context, ...args) {
+    if (typeof this !== 'function') {
+      throw new TypeError('error');
+    }
     if (this === Function.prototype) {
       return undefined; // 用于防止 Function.prototype.myCall() 直接调用
     }
@@ -133,6 +121,59 @@ Function.prototype.myBind = function(context) {
 
   Function.prototype.myCall = myCall;
 })();
+
+~(function() {
+  function myApply(context, args) {
+    if (typeof this !== 'function') {
+      throw new TypeError('error');
+    }
+    if (this === Function.prototype) {
+      return undefined; // 用于防止 Function.prototype.myCall() 直接调用
+    }
+
+    context = context || window;
+    let type = typeof context;
+    if (!/^(object|function)$/.test(type)) {
+      if (/^(symbol|bigint)$/.test(type)) {
+        context = Object(context);
+      } else {
+        context = new context.constructor(context);
+      }
+    }
+    let key = Symbol('key'),
+      result;
+    context[key] = this;
+    // 处理参数和 call 有区别
+    if (Array.isArray(args)) {
+      result = context[key](...args);
+    } else {
+      result = context[key]();
+    }
+    delete context[key];
+    return result;
+  }
+
+  Function.prototype.myApply = myApply;
+})();
+
+Function.prototype.myBind = function(context) {
+  if (typeof this !== 'function')
+    throw new TypeError(
+      'Function.prototype.bind - what is trying to be bound is not callable'
+    );
+  if (this === Function.prototype) throw new TypeError('error');
+  // 保存this
+  const _this = this;
+  const args = [...arguments].slice(1); // args = Array.prototype.slice.call(arguments, 1);
+  // 返回一个函数
+  return function F() {
+    // 因为返回了一个函数，我们可以 new F()，所以需要判断是否用于构造函数
+    if (this instanceof F) {
+      return new _this(...args, ...arguments);
+    }
+    return _this.apply(context, args.concat(...arguments));
+  };
+};
 
 /**
  * 打印数组每一项
