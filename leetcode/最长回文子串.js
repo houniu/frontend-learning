c20588ee6086756897b4c1ff7fd5e475817eef9b
侯磊 <houlei602953@pwrd.com>
2020-10-20 14:43:00 +0800

    add leetcode/longest-palindromic-substring

--- leetcode/最长回文子串.js
+++ leetcode/最长回文子串.js
@@ -1,7 +1,7 @@
 /*
  * [5] 最长回文子串
  *
- * https://leetcode.com/problems/longest-palindromic-substring/
+ * https://leetcode.com/problems/lonleegest-palindromic-substring/
  *
  * @description:
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
@@ -15,3 +15,51 @@
  * 输入: "cbbd"
  * 输出: "bb"
  */
+
+/**
+ * 判断是否是回文字符串
+ * @param {String} str
+ */
+const isPalindrome = str => {
+  let len = str.length,
+    middleIndex = parseInt(len / 2);
+  for (let i = 0; i < middleIndex; i++) {
+    if (str[i] !== str[len - i - 1]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+/**
+ * 采用暴力枚举法
+ * @param {*String} s
+ */
+export const longestPalindrome = s => {
+  if (!s || s.length < 2) {
+    return s;
+  }
+  let result = '',
+    len = s.length;
+  for (let i = 0; i < len; i++) {
+    for (let j = i + 1; j <= len; j++) {
+      const temp = s.slice(i, j);
+      if (
+        temp
+          .split('')
+          .reverse()
+          .join('') === temp &&
+        temp.length > result.length
+      ) {
+        result = temp;
+      }
+    }
+  }
+
+  return result;
+};
+
+export default {
+  isPalindrome,
+  longestPalindrome,
+};
