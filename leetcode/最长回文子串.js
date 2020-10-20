0987f5b0611790a6334f353e01dd05c59135db26
侯磊 <houlei602953@pwrd.com>
2020-10-20 14:44:46 +0800

    add leetcode/longest-palindromic-substring

--- leetcode/最长回文子串.js
+++ leetcode/最长回文子串.js
@@ -1,7 +1,7 @@
 /*
  * [5] 最长回文子串
  *
- * https://leetcode.com/problems/lonleegest-palindromic-substring/
+ * https://leetcode.com/problems/longest-palindromic-substring/
  *
  * @description:
  * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
